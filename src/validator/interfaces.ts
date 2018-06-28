b707dc6d1a7cb00206bcfdb0c48e2178bf390dea
rodrigo debian <correo.rodrigo@gmail.com>
2018-06-28 13:32:30 -0500

    added first classes

---
+++ src/validator/interfaces.ts
@@ -0,0 +1,18 @@
+interface IValidatable {
+    validate(): Promise<boolean>;
+}
+
+interface IValidationResult {
+    isValid: boolean;
+    message: string;
+}
+
+interface IFieldValidator<T> {
+    check(value?: T): Promise<IValidationResult>;
+}
+
+export {
+    IValidatable,
+    IValidationResult,
+    IFieldValidator
+}
