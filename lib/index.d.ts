643fb0454196acbb9f764da539f45beb442c4a03
rodrigo debian <correo.rodrigo@gmail.com>
2018-07-03 15:00:12 -0500

    refactored

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,10 +1,2 @@
-import * as validatorInterfaces from './validator/interfaces';
-import * as fieldInterfaces from './field/interfaces';
-import koForm = require("./form/koForm");
-export declare class Valiko {
-    /**
-     * Creates a new form.
-     * @param validators List of validation rules.
-     */
-    static createForm<T>(validators: validatorInterfaces.IFieldValidator<fieldInterfaces.IFieldBase<any, any>[]>[]): koForm;
-}
+import KoForm = require("./form/koForm");
+export { KoForm };
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
 import * as validatorInterfaces from './validator/interfaces';
 import * as fieldInterfaces from './field/interfaces';
-import koForm = require("./form/koForm");
+import KoForm = require("./form/koForm");
 
-export class Valiko {
-    /**
-     * Creates a new form.
-     * @param validators List of validation rules.
-     */
-    public static createForm<T>(validators: validatorInterfaces.IFieldValidator<fieldInterfaces.IFieldBase<any, any>[]>[]): koForm {
-        let form = new koForm();
-        return form;
-    }
-}
+export {
+    KoForm
+};
 
