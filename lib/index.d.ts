1b02f5c63fab1a9e4525949dc55daa6b32f7536a
rodrigo debian <correo.rodrigo@gmail.com>
2018-07-09 17:05:29 -0500

    added fields

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,9 @@
 import { IField, IFieldArray, IFieldBase } from "./field/interfaces";
+import Field from "./field/field";
+import FieldArray from "./field/field";
+import FieldBase from "./field/field";
 import { IValidatable, IValidationResult, IFieldValidator } from "./validator/interfaces";
-import KoForm = require("./form/koForm");
+import KoForm from "./form/koForm";
 import ValidatorBase from "./validator/validatorBase";
-export { KoForm, ValidatorBase, IField, IFieldArray, IFieldBase, IValidatable, IValidationResult, IFieldValidator };
+import ValidatableValidator from './validator/validatableValidator';
+export { FieldBase, Field, FieldArray, KoForm, ValidatorBase, ValidatableValidator, IField, IFieldArray, IFieldBase, IValidatable, IValidationResult, IFieldValidator };
--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,31 @@ import {
     IFieldBase
 } from "./field/interfaces";
 
+
+import Field from "./field/field";
+import FieldArray from "./field/field";
+import FieldBase from "./field/field";
+
 import {
     IValidatable,
     IValidationResult,
     IFieldValidator
 } from "./validator/interfaces";
 
-import KoForm = require("./form/koForm");
+import KoForm from "./form/koForm";
 import ValidatorBase from "./validator/validatorBase";
+
+import ValidatableValidator from   './validator/validatableValidator';
 
 
 export {
+    FieldBase,
+    Field,
+    FieldArray,
     // Concrete Classes
     KoForm,
     ValidatorBase,
+    ValidatableValidator,
     // Field Interfaces
     IField,
     IFieldArray,
