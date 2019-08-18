594715a8f1671e3c33156afde06b7417fe12ce62
Rodrigo Debian <rosec@protonmail.ch>
2019-08-18 12:40:16 -0500

    removing nodemon

--- test/fieldTest.js
+++ test/fieldTest.js
@@ -2,12 +2,22 @@ var assert = require("assert");
 var vko = require('../lib');
 global.ko = require('knockout');
 
-describe('Field test', function() {
-    describe('init', function() {
-	it('is field initialized', function(done) {
-	    var fld = new vko.Field();
-	    console.log(fld);
-	    done();
-	});
+describe('Field', function() {
+    describe('constructor', function() {
+		it('initialized() should be false for new obj', function(done) {
+			var fld = new vko.Field([]);
+			assert.ok(fld.initialized() === false);	
+			assert.ok(fld.hasError() === false);
+			assert.equal(fld.value(), undefined);
+			done();
+		});
+		it('initialized() should be true after value chage', function(done) {
+			var fld = new vko.Field([]);
+			fld.value(1);
+			assert.ok(fld.initialized());
+			assert.ok(fld.hasError() === false);
+			assert.equal(fld.value(), 1);
+			done();
+		});
     });
 });
